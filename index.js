cffc4b3f9c7dbe73e16d24c914e0b586f2cb50de
Atul R <atulanand94@gmail.com>
2020-08-14 23:57:38 +0200

    upgrade to vercel

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const { google } = require('googleapis');
-const result = require('lodash.result');
-const redirect = require('micro-redirect');
-const url = require('url');
-const atob = require('atob');
+const { google } = require("googleapis");
+const result = require("lodash.result");
+const redirect = require("micro-redirect");
+const url = require("url");
+const atob = require("atob");
 
-const START_DATE = '2017-12-01';
+const START_DATE = "2017-12-01";
 
 const gaServiceAccount = {
-  type: 'service_account',
-  project_id: 'rnbook-analytics',
+  type: "service_account",
+  project_id: "rnbook-analytics",
   client_id: process.env.GA_CLIENT_ID,
   private_key_id: process.env.GA_PRIVATE_KEY_ID,
   private_key: atob(process.env.GA_PRIVATE_KEY),
-  client_email: 'analyticsbot@rnbook-analytics.iam.gserviceaccount.com',
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_email: "analyticsbot@rnbook-analytics.iam.gserviceaccount.com",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url:
-    'https://www.googleapis.com/robot/v1/metadata/x509/analyticsbot%40rnbook-analytics.iam.gserviceaccount.com',
+    "https://www.googleapis.com/robot/v1/metadata/x509/analyticsbot%40rnbook-analytics.iam.gserviceaccount.com",
 };
 
 const getGAMetricsRawData = async (key, viewId) => {
@@ -25,8 +25,8 @@ const getGAMetricsRawData = async (key, viewId) => {
     key.client_email,
     null,
     key.private_key,
-    ['https://www.googleapis.com/auth/analytics.readonly'],
-    null,
+    ["https://www.googleapis.com/auth/analytics.readonly"],
+    null
   );
 
   return new Promise((resolve, reject) => {
@@ -34,17 +34,17 @@ const getGAMetricsRawData = async (key, viewId) => {
       if (error) {
         return reject(error);
       }
-      const analytics = google.analyticsreporting('v4');
+      const analytics = google.analyticsreporting("v4");
       const options = {
         auth: jwtClient,
         resource: {
           reportRequests: [
             {
               viewId,
-              dateRanges: [{ startDate: START_DATE, endDate: 'today' }],
+              dateRanges: [{ startDate: START_DATE, endDate: "today" }],
               metrics: [
-                { expression: 'ga:pageviews' },
-                { expression: 'ga:users' },
+                { expression: "ga:pageviews" },
+                { expression: "ga:users" },
               ],
             },
           ],
@@ -63,20 +63,21 @@ const getGAMetricsRawData = async (key, viewId) => {
 const extractMetrics = (raw) => {
   const gaMetrics = result(
     raw,
-    'data.reports[0].data.rows[0].metrics[0].values',
-    [],
+    "data.reports[0].data.rows[0].metrics[0].values",
+    []
   );
   const [pageviews, users] = gaMetrics;
   return { pageviews, users };
 };
 
 const getAnalytics = async () => {
-  const viewId = '166250965';
+  const viewId = "166250965";
   const data = await getGAMetricsRawData(gaServiceAccount, viewId);
   return extractMetrics(data);
 };
 
-const generateUrl = (title, value, color) => `https://img.shields.io/badge/${title}-${value}-${color}.svg`;
+const generateUrl = (title, value, color) =>
+  `https://img.shields.io/badge/${title}-${value}-${color}.svg`;
 
 const getRoute = (urlstring) => {
   const { pathname } = url.parse(urlstring, true);
@@ -88,12 +89,12 @@ module.exports = async (req, res) => {
   const route = getRoute(req.url);
 
   let location = null;
-  if (route === '/pageviews') {
-    location = generateUrl('Page Views', pageviews, 'blue');
-  } else if (route === '/users') {
-    location = generateUrl('Users', users, 'green');
+  if (route === "/pageviews") {
+    location = generateUrl("Page Views", pageviews, "blue");
+  } else if (route === "/users") {
+    location = generateUrl("Users", users, "green");
   } else {
-    location = generateUrl('404', 'not found', 'red');
+    location = generateUrl("404", "not found", "red");
   }
   const statusCode = 302;
   redirect(res, statusCode, location);
